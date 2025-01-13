801c5049d334ec091aadeb31183e04d577dc2fc4
flexteck <flexteckse@gmail.com>
2025-01-13 14:58:08 +0100

    webpack commit

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,8 @@
+import $ from "jquery";
+
+$(document).ready(function () {
+  $("body").append("<p>ALX Dashboard</p>");
+  $("body").append("<p>Dashboard data for the students</p>");
+  $("body").append("<p>Copyright - ALX</p>");
+});
+
